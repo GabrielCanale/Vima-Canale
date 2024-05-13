7ff9f203a14b91b3e110c243826eb9a6e3fe7070
GabrielCanale <132526474+GabrielCanale@users.noreply.github.com>
2024-05-13 18:47:53 -0300

    Add files via upload

---
+++ VIMA-Vue.js-main/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home-view">
+    <center><h1>Bem-vindo à página inicial</h1></center>
+    <button type="button" class="btn btn-info">Logado</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
